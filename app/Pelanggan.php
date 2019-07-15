e84e76470f5b742b82069b81623120ed52adf234
Muhamad Rizky <mrizky019@gmail.com>
2019-07-15 11:35:36 +0700

    update delete pelanggan & CRUD pegawai

--- app/Pelanggan.php
+++ app/Pelanggan.php
@@ -9,6 +9,7 @@ class Pelanggan extends Model
 	public $table = 'pelanggan';
 	public $timestamps = false;
 	protected $primaryKey = 'id_pelanggan';
+	public $incrementing = false;
 
     protected $fillable = [
     	'id_pelanggan', 'email', 'nama', 'no_telepon', 'alamat'
