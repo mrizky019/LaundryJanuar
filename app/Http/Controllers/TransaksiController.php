8359b802bd6abfca7031a83590ee400eb9dec231
nauhalf <naufal.kotori@outlook.com>
2019-07-27 22:34:10 +0700

    qq

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -207,6 +207,10 @@ class TransaksiController extends Controller
 				$client = new Client(['http_errors' => false]);
 				
 				$response_transaksi = $client->get("http://".$value.":8000/api/transaksi/other_server/get_transaksi_laundry_other_server");
+				
+				var_dump($response_transaksi->getStatusCode());
+				die();
+
 				if($response_transaksi->getStatusCode()==200){
 					$content_transaksi = json_decode($response_transaksi->getBody()->getContents());
 					
