d638ead4d9323da991f74249170d5c41573a29f9
nauhalf <naufal.kotori@outlook.com>
2019-07-27 15:18:54 +0700

    transaksi2

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -295,74 +295,6 @@ class TransaksiController extends Controller
 			catch(\Exception $e){
 
 			}
-
-			// try{
-			// 	$response_transaksi = $client->get("http://".$value.":8000/api/transaksi/other_server/get_detail_transaksi_laundry_other_server");
-
-			// 	if($response_transaksi->getStatusCode()==200){
-			// 		$content_transaksi = json_decode($response_transaksi->json());
-
-			// 		$where = array();
-			// 		foreach($content_transaksi as $d){
-			// 			array_push($where, [
-			// 				"id_server" => $d["id_server"],
-			// 				"id_detail_laundry" => $d["id_detail_laundry"]
-			// 			]);
-			// 		}
-
-			// 		if(!empty($where)){
-			// 			$get = DB::table('detail_laudry')
-			// 					->whereRaw(
-			// 						whereNotInMultipleColumn(
-			// 							["id_server", "id_detail_laundry"], 
-			// 							$where
-			// 							)
-			// 						)->get()->toArray();
-			// 			if(!empty($get)){
-			// 				DB::table('detail_laundry')
-			// 					->insert($get);
-			// 			}
-
-			// 		}
-			// 	}		
-			// }
-			// catch(\Exception $e){
-
-			// }
-
-			// try{
-			// 	$response_transaksi = $client->get("http://".$value.":8000/api/aktivitaslaundry/other_server/get_aktivitas_laundry_other_server");
-
-			// 	if($response_transaksi->getStatusCode()==200){
-			// 		$content_transaksi = json_decode($response_transaksi->json());
-
-			// 		$where = array();
-			// 		foreach($content_transaksi as $d){
-			// 			array_push($where, [
-			// 				"id_server" => $d["id_server"],
-			// 				"id_aktivitas_laundry" => $d["id_aktivitas_laundry"]
-			// 			]);
-			// 		}
-
-			// 		if(!empty($where)){
-			// 			$get = DB::table('aktivitas_laundry')
-			// 					->whereRaw(
-			// 						whereNotInMultipleColumn(
-			// 							["id_server", "id_aktivitas_laundry"], 
-			// 							$where
-			// 							)
-			// 						)->get()->toArray();
-			// 			if(!empty($get)){
-			// 				DB::table('aktivitas_laundry')
-			// 					->insert($get);
-			// 			}
-
-			// 		}
-			// 	}		
-			// }
-			// catch(\Exception $e){
-
-			// }
 			
 		}
 
@@ -382,7 +314,8 @@ class TransaksiController extends Controller
 	}
 
 	public function paid(Request $request){
-		$paid = DB::statement("CALL procedure_paid_laundry(:id_transaksi)", array(
+		$paid = DB::statement("CALL procedure_paid_laundry(:id_server, :id_transaksi)", array(
+			"id_server" => $request->id_server,			
 			"id_transaksi" => $request->id_transaksi
 		));
 
@@ -432,6 +365,107 @@ class TransaksiController extends Controller
 	}
 
 	public function showFinishedTransaction(Request $request){
+
+		$other_ip = get_server($request->id_server); //GuzzleHttp\Client
+
+
+		foreach($other_ip as $key => $value){
+			try{
+				$self_transaksi = DB::table('transaksi_laundry')->select('id_server', 'id_transaksi_laundry')->get();
+
+				$client = new Client(['http_errors' => false]);
+				
+				$response_transaksi = $client->get("http://".$value.":8000/api/transaksi/other_server/get_transaksi_laundry_other_server");
+				if($response_transaksi->getStatusCode()==200){
+					$content_transaksi = json_decode($response_transaksi->getBody()->getContents());
+					
+				
+					$where = array();
+					foreach($content_transaksi->data as $d){
+
+						$arrayData = (array)$d;
+						$exists = DB::table('transaksi_laundry')
+						->where("id_server", $d->id_server)->where("id_transaksi_laundry",$d->id_transaksi_laundry)->first();
+
+						if($exists == null){
+							DB::table("transaksi_laundry")
+								->insert(
+									$arrayData	
+								);
+						} else {
+
+						}
+
+					}
+					$response_detail_transaksi = $client->get("http://".$value.":8000/api/transaksi/other_server/get_detail_transaksi_laundry_other_server");
+					if($response_detail_transaksi->getStatusCode()==200){
+						$content_detail_transaksi = json_decode($response_detail_transaksi->getBody()->getContents());
+						
+					
+						$where = array();
+						foreach($content_detail_transaksi->data as $det){
+
+							$arrayDataDetail = (array)$det;
+							$exists = DB::table('detail_laundry')
+							->where("id_server", $det->id_server)->where("id_detail_laundry",$det->id_detail_laundry)->first();
+
+							if($exists == null){
+								DB::table("detail_laundry")
+									->insert(
+										$arrayDataDetail
+									);
+							} else {
+
+							}
+
+						}
+
+						$response_detail_transaksi = $client->get("http://".$value.":8000/api/aktivitaslaundry/other_server/get_aktivitas_laundry_other_server");
+						if($response_detail_transaksi->getStatusCode()==200){
+							$content_aktivitas = json_decode($response_detail_transaksi->getBody()->getContents());
+							
+						
+							$where = array();
+							foreach($content_aktivitas->data as $akt){
+
+								$arrayDataAktivitas = (array)$akt;
+								$exists = DB::table('aktivitas_laundry')
+								->where("id_server", $akt->id_server)->where("id_aktivitas_laundry",$akt->id_aktivitas_laundry)->first();
+
+								if($exists == null){
+									DB::table("aktivitas_laundry")
+										->insert(
+											$arrayDataAktivitas
+										);
+								} else {
+									if($akt->updated_at != null){
+										if($exists->updated_at != null){
+											if(strtotime($exists->updated_at) < strtotime($akt->updated_at)){
+												DB::table("aktivitas_laundry")
+													->where('id_server', $akt->id_server)
+													->where('id_aktivitas_laundry', $akt->id_aktivitas_laundry)
+													->update($arrayDataAktivitas);
+											}
+										} else {
+											$update = DB::table("aktivitas_laundry")
+													->where('id_server', $akt->id_server)
+													->where('id_aktivitas_laundry', $akt->id_aktivitas_laundry)
+													->update($arrayDataAktivitas);
+											
+										}
+									}
+								}
+							}
+						}
+					}		
+				}
+			}
+			catch(\Exception $e){
+
+			}
+			
+		}
+
 		$result = DB::table('view_laporan_transaksi_cabang')
 		->where('id_cabang', $request->id_cabang)
 		->where('is_paid', 1)
